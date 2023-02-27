43efbfc44a8cfef6a8cb002f779cacd19408b8f0
Tom Settle <tom.k.settle@gmail.com>
2023-02-27 14:10:10 -0600

    Completed.

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -14,21 +14,23 @@ namespace LoggingKata
         {
             // TODO:  Find the two Taco Bells that are the furthest from one another.
             // HINT:  You'll need two nested forloops ---------------------------
-
+            
             logger.LogInfo("Log initialized");
 
             // use File.ReadAllLines(path) to grab all the lines from your csv file
             // Log and error if you get 0 lines and a warning if you get 1 line
             var lines = File.ReadAllLines(csvPath);
-
-            logger.LogInfo($"Lines: {lines[0]}");
+            logger.LogInfo($"Lines read: {lines.Length}");
 
             // Create a new instance of your TacoParser class
-            var parser = new TacoParser();
+            var parser = new TacoParser(logger);
 
             // Grab an IEnumerable of locations using the Select command: var locations = lines.Select(parser.Parse);
-            var locations = lines.Select(parser.Parse).ToArray();
+            //var locations = lines.Select(parser.Parse).ToArray();
+            var stores = lines.Select(x => parser.Parse(x,false)).ToArray();
+            logger.LogInfo($"Lines parsed: {stores.Length}");
 
+            #region INSTRUCTIONS
             // DON'T FORGET TO LOG YOUR STEPS
 
             // Now that your Parse method is completed, START BELOW ----------
@@ -51,9 +53,29 @@ namespace LoggingKata
             // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
 
             // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
+            #endregion
 
+            int firstTH = -1;
+            int secondTH = -1;
+            double maxDistance = 0;
 
-            
+            //Find the two locations that are the farthest apart
+            for (int i = 0; i < stores.Length - 1; i++)
+            {
+                var firstStore = new GeoCoordinate(stores[i].Location.Latitude, stores[i].Location.Longitude);
+                for (int j = i + 1; j < stores.Length; j++)
+                {
+                    var secondStore = new GeoCoordinate(stores[j].Location.Latitude, stores[j].Location.Longitude);
+                    double thisDist = firstStore.GetDistanceTo(secondStore);
+                    if (thisDist > maxDistance)
+                    {
+                        maxDistance = thisDist;
+                        firstTH = i;
+                        secondTH = j;
+                    }
+                }
+            }
+            logger.LogInfo($"The most distant stores are: {stores[firstTH].Name} and {stores[secondTH].Name} with a distance of {maxDistance*100/2.54/12/5280}miles");
         }
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,44 +1,68 @@
-﻿namespace LoggingKata
+﻿using System;
+
+namespace LoggingKata
 {
     /// <summary>
     /// Parses a POI file to locate all the Taco Bells
     /// </summary>
     public class TacoParser
     {
-        readonly ILog logger = new TacoLogger();
-        
-        public ITrackable Parse(string line)
-        {
-            logger.LogInfo("Begin parsing");
+        ILog thisLogger;
+        int errCounter = 0;
 
+        public TacoParser() 
+        { 
+            thisLogger = new TacoLogger(); 
+        }
+        public TacoParser(ILog activeLogger) 
+        {  
+            thisLogger = activeLogger; 
+        }
+        
+        public ITrackable Parse(string line, bool withLogging=true)
+        {
             // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
             var cells = line.Split(',');
 
-            // If your array.Length is less than 3, something went wrong
-            if (cells.Length < 3)
+            if (errCounter > 5)
+            {
+                throw new Exception("Error count exceeded.");
+            }
+            else if (cells.Length != 3)                                      // If your array.Length is less than 3, something went wrong
             {
                 // Log that and return null
                 // Do not fail if one record parsing fails, return null
+                ++errCounter;
+                if (withLogging) thisLogger.LogInfo("Invalid column layout.");
                 return null; // TODO Implement
             }
-
-            // grab the latitude from your array at index 0
-            // grab the longitude from your array at index 1
-            // grab the name from your array at index 2
-
-            // Your going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
-
-            // You'll need to create a TacoBell class
-            // that conforms to ITrackable
-
-            // Then, you'll need an instance of the TacoBell class
-            // With the name and point set correctly
-
-            // Then, return the instance of your TacoBell class
-            // Since it conforms to ITrackable
-
-            return null;
+            else
+            {
+                try
+                {
+                    double lat = double.Parse(cells[0]);                // grab the latitude from your array at index 0
+                    double lon = double.Parse(cells[1]);                // grab the longitude from your array at index 1
+                    string nm = cells[2];                               // grab the name from your array at index 2
+                    // Your going to need to parse your string as a `double` which is similar to parsing a string as an `int`
+                    // You'll need to create a TacoBell class that conforms to ITrackable
+                    // Then, you'll need an instance of the TacoBell class with the name and point set correctly
+                    // Then, return the instance of your TacoBell class since it conforms to ITrackable
+                    if (withLogging) thisLogger.LogInfo("Successfully parsed.");
+                    return new TacoHell() { Name = nm, Location = new Point() { Latitude = lat, Longitude = lon } };
+                }
+                catch (Exception e)
+                {
+                    ++errCounter;
+                    if (withLogging) thisLogger.LogError($"Failed to parse ({line})",e);
+                    return null;
+                }
+            }
         }
+    }
+
+    public class TacoHell : ITrackable
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
     }
 }
